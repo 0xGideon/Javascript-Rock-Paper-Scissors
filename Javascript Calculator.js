905e77f726062663a285b3d96cc0c85c1a843fcc
0xGideon <remmyogunsol@gmail.com>
2025-09-09 09:20:37 +0100

    Add Javascript Calculator.js

---
+++ Javascript Calculator.js
@@ -0,0 +1,42 @@
+//Rock Paper and Scissors game.
+//0 beats 2
+//1 beats 0
+//2 beats 1
+
+const gameArr = ["Rock", "Paper", "Scissors"];
+let player = prompt("Enter Rock, Paper, Scissors");
+
+switch(player) {
+case "Rock":
+case "Paper":
+case "Scissors":
+let playerIndex = gameArr.lastIndexOf(player);
+let computer = Math.random() * 3;
+computerIndex = Math.floor(computer);
+console.log(`You choose: ${gameArr[playerIndex]} and Computer choose: ${gameArr[computerIndex]}`);
+if ((playerIndex === 0) && (computerIndex ===  2)) {
+console.log("You win");
+}
+else if ((playerIndex === 1) && (computerIndex === 0)) {
+console.log("You win");
+}
+else if ((playerIndex === 2) && (computerIndex === 1)) {
+console.log("You win");
+}
+else if ((playerIndex === 2) && (computerIndex === 0)) {
+console.log("You loose, Computer Win");
+}
+else if ((playerIndex === 0) && (computerIndex === 1)) {
+console.log("You loose, Computer Wins");
+}
+else if ((playerIndex === 1) && (computerIndex === 2)) {
+console.log("You loose, Computer Win");
+}
+else {
+console.log("Its a tie");
+}
+break;
+
+default:
+console.log("Enter a valid input (Rock, Paper, Scissors)");
+} //end
